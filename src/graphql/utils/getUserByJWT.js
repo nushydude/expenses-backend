99fe3250f8039c0a6664dc11fe2bde02be52301e
Anusha <cdan.dharmasena@gmail.com>
2019-08-15 18:38:45 +1000

    flow

--- src/graphql/utils/getUserByJWT.js
+++ src/graphql/utils/getUserByJWT.js
@@ -1,7 +1,7 @@
 // @flow
 import cuid from 'cuid';
 
-export async function getUserByJWT(jwt) {
+export async function getUserByJWT(jwt: string) {
   // TODO:
   // - validate jwt
   // - find user in DB and return
